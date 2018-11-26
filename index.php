91b3f06a5312760127f424c5b92aa30ad9fa6985
magavel <avel.feron@gmail.com>
2018-11-26 11:51:21 +0100

    maj

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+    <link rel="stylesheet" type="text/css" href="css/mon_site.css">
+</head>
+<body style="background-image: url('img/accueil.jpg')">
+<div class="content">
+
+    <div id="nav">
+
+        <?php include_once ('menu.php');?>
+
+
+    </div> <!-- fin menu-->
+    <div class="page">
+        <h1>Accueil</h1>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,16 @@
+<?php include('img/sprite_symbol_pictos.svg') ?>
+<div id="menuIcone">
+            <a href="#"><svg class="Icon"><use xlink:href="#hamburger"></use></svg></a>
+        </div>
+        <div id="menuText">
+            <ul>
+                <li><a href="#"> <svg class="Icon"><use xlink:href="#home"></use><span class="texteMenu">Accueil</span> </a> </li>
+                <li><a href="#"> <svg class="Icon"><use xlink:href="#cerise"></use></svg><span class="texteMenu">Cerise</span></a> </li>
+                <li><a href="#"> <svg class="Icon"><use xlink:href="#poire"></use><span class="texteMenu">Poire</span></svg></a> </li>
+                <li><a href="pomme.php"> <svg class="Icon"><use xlink:href="#pomme"></use></svg><span class="texteMenu">Pomme</span></a> </li>
+                <li><a href="#"> <svg class="Icon"><use xlink:href="#tomate"></use></svg><span class="texteMenu">Tomate</span></a> </li>
+
+
+            </ul>
+        </div>
+
